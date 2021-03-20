96d6693eec9e69f7e22476dc993513e20203123a
Daniel <danielrs9504@gmail.com>
2021-03-20 16:48:25 -0500

    Crea el modelo de products

---
+++ db/migrate/20210320213445_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.text :photo
+      t.decimal :price
+      t.datetime :auctionDate
+      t.datetime :startingDate
+      t.datetime :endingDate
+
+      t.timestamps
+    end
+  end
+end
