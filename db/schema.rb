f6fefcbfcf5dd186049fd76db5a59aa7f9d5daec
Daniel <danielrs9504@gmail.com>
2021-03-21 15:57:12 -0500

    Agrega columna name al modelo de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_200401) do
+ActiveRecord::Schema.define(version: 2021_03_21_205702) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_03_21_200401) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
     t.string "phone"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
