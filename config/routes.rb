a5311bad213d5ff46516c191341cc5160d9ed9e2
Daniel <danielrs9504@gmail.com>
2021-03-21 10:17:11 -0500

    Modifica rutas y landing de la aplicacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get '/products' => 'products#index'
-  get '/products/new' => 'products#new', as: 'new_product'
-  post '/products' => 'products#create'
-  get '/products/:id' => 'products#show', as: 'product'
-  get '/products/:id/edit' => 'products#edit', as: 'edit_product'
-  patch '/products/:id' => 'products#update'
-  delete '/products/:id' => 'products#delete'
+  # get '/products' => 'products#index'
+  # get '/products/new' => 'products#new', as: 'new_product'
+  # post '/products' => 'products#create'
+  # get '/products/:id' => 'products#show', as: 'product'
+  # get '/products/:id/edit' => 'products#edit', as: 'edit_product'
+  # patch '/products/:id' => 'products#update'
+  # delete '/products/:id' => 'products#delete'
+
+  # Todas las rutas anteriores, se pueden reemplazar exactamente con la siguiente linea
+  resources :products
+
+  # Landing de la app
+  root 'products#index'
 end
