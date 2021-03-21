cf03afed5949500fdbf9566180d7052c4ee1fd64
Daniel <danielrs9504@gmail.com>
2021-03-21 10:08:50 -0500

    Agrega funcionalidad para eliminar productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,15 @@ class ProductsController < ApplicationController
     end
   end
 
+  def delete
+    # Sin @ porque no usaremos la variable en la vista
+    product = Product.find(params[:id])
+    product.destroy
+
+    redirect_to products_path, notice: "Product deleted"
+  end
+
+
   private
     def product_params
       params.require(:product).permit(:name, :description, :image, :price, :auctionDate, :startingDate, :endingDate)
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/products/:id' => 'products#details', as: 'product'
   get '/products/:id/edit' => 'products#edit', as: 'edit_product'
   patch '/products/:id' => 'products#update'
+  delete '/products/:id' => 'products#delete', as: 'delete_product'
 end
