9249c5804c8e76923aef1af7f3885049011094bc
Daniel <danielrs9504@gmail.com>
2021-03-21 16:42:18 -0500

    Agrega annotate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :integer          not null, primary key
+#  auctionDate  :datetime
+#  description  :text
+#  endingDate   :datetime
+#  image        :text
+#  name         :string
+#  price        :decimal(, )
+#  startingDate :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Product < ApplicationRecord
 end
